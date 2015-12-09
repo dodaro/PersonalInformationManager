380b8e33521f22f35e3cd016998313806c3ab403
Mostaf <Mostaf@Most>
2015-12-09 12:33:15 +0100

    PassGen

--- EaPersonalInformation/src/main/java/it/unical/ea/model/PwdGen.java
+++ EaPersonalInformation/src/main/java/it/unical/ea/model/PwdGen.java
@@ -9,20 +9,6 @@ public class PwdGen {
 	public PwdGen() {
 	}
 
-	public String onlylettersandnumbers() {
-		StringBuffer buffer = new StringBuffer();
-		int length = (int) ((Math.random()*46)+4);
-		String allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890";
-		char[] alphabet = allowedCharacters.toCharArray();
-
-		for (int i = 0; i < length; i++) {
-			buffer.append(alphabet[random.nextInt(alphabet.length)]);
-		}
-
-		password = buffer.toString();
-		return password;
-	}
-
 	public String onlyletters() {
 		StringBuffer buffer = new StringBuffer();
 		int length = (int) ((Math.random()*50)+4);
@@ -37,7 +23,33 @@ public class PwdGen {
 		return password;
 	}
 	
+	public String onlylettersandnumbers() {
+		StringBuffer buffern = new StringBuffer();
+		int lengthn = (int) ((Math.random()*50)+4);
+		String allowedCharactersn = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		char[] alphabetn = allowedCharactersn.toCharArray();
+
+		for (int i = 0; i < lengthn; i++) {
+			buffern.append(alphabetn[random.nextInt(alphabetn.length)]);
+		}
+
+		password = buffern.toString();
+		return password;
+	}
 	
+	public String LetNumSpecialChar() {
+		StringBuffer buffern = new StringBuffer();
+		int lengthn = (int) ((Math.random()*50)+4);
+		String allowedCharactersn = "~!@#$%^&*()`_-+=/.,><;][{}1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		char[] alphabetn = allowedCharactersn.toCharArray();
+
+		for (int i = 0; i < lengthn; i++) {
+			buffern.append(alphabetn[random.nextInt(alphabetn.length)]);
+		}
+
+		password = buffern.toString();
+		return password;
+	}
 	@Override
 	public String toString() {
 		return "password: " + password;
